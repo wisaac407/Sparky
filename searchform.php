ae349002b2180c4b543fbeb91c2b2c812c8b9935
Isaac Weaver <wisaac407@gmail.com>
2014-10-04 16:43:19 -0500

    Bootstrap Integration: Re-designed the search form.
    Now the search form looks much better and is entirly bootstrap :)

--- searchform.php
+++ searchform.php
@@ -1,9 +1,13 @@
-<form action="<?php bloginfo('siteurl'); ?>" class="search-form">
-    <div class="input-wrap search">
-        <label for="input-s" class="screen-reader-text">Search for:</label>
-        <input type="search" id="input-s" name="s" />
-    </div>
-    <div class="input-wrap submit">
-        <input type="submit" value="Search" class="button" />
+<form action="<?php bloginfo('url'); ?>" class="search-form">
+	<div class="form-group">
+		<div class="input-group">
+			<label for="input-s" class="sr-only">Search for:</label>
+        	<input type="search" id="input-s" name="s" class="form-control" />
+        	<div class="input-group-btn">
+				<button class="btn btn-default">
+					<i class="fa fa-search"></i>
+				</button>
+			</div>
+		</div>
 	</div>
 </form>
