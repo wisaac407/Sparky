de56fb7343f1bd4f2e624b789a0432ab4679ee56
Isaac Weaver <wisaac407@gmail.com>
2014-10-04 16:34:20 -0500

    Adding browser to body class.
    This will allow us to create better cross platform stability.

--- functions.php
+++ functions.php
@@ -28,6 +28,22 @@ function theme_enqueue_scripts(){
 //Add Featured Image Support
 add_theme_support('post-thumbnails');
 
+// Add Browser to body class
+add_filter('body_class','browser_body_class');
+function browser_body_class($classes) {
+	global $is_lynx, $is_gecko, $is_IE, $is_opera, $is_NS4, $is_safari, $is_chrome, $is_iphone;
+	if($is_lynx) $classes[] = 'lynx';
+	elseif($is_gecko) $classes[] = 'gecko';
+	elseif($is_opera) $classes[] = 'opera';
+	elseif($is_NS4) $classes[] = 'ns4';
+	elseif($is_safari) $classes[] = 'safari';
+	elseif($is_chrome) $classes[] = 'chrome';
+	elseif($is_IE) $classes[] = 'ie';
+	else $classes[] = 'unknown';
+	if($is_iphone) $classes[] = 'iphone';
+	return $classes;
+}
+
 // Clean up the <head>
 function removeHeadLinks() {
 	remove_action('wp_head', 'rsd_link');
